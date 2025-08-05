d9a1446c7e8d23d100e5b2f74033ae7763ef2f13
Jaber Allawnah <s12112071@stu.najah.edu>
2025-08-05 17:28:06 +0300

    Time: 0 ms (100.00%) | Memory: 12.9 MB (47.90%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -17,7 +17,7 @@ public:
         if(!p||!q) return false;
         if(p->val!=q->val) return false;
 
-        return isSameTree(p->right,q->right)&&isSameTree(p->left,q->left);
+        return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
      
 
     }
