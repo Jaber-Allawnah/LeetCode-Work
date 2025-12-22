9a3e74dd67fcdd06d7f3e9a1089834a540491f6c
Jaber Allawnah <s12112071@stu.najah.edu>
2025-12-22 13:57:35 +0200

    Time: 3 ms (67.77%) | Memory: 14.7 MB (53.23%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> final=nums;
-        vector <int> answer;
-        sort(final.begin(),final.end());
-        int i=0;
-        int j=nums.size()-1;
-        while(1){
-            if(final[i]+final[j]==target){
-                break;
-            }
-            else if(final[i]+final[j]>target){
-                j--;
-            }
-            else {
-                i++;
-            }
-
+        unordered_map <int,int> myMap;
+        for(int i=0;i<nums.size();i++){
+            int compliment=target-nums[i];
+            if(myMap.count(compliment))
+            return {i,myMap[compliment]};
+            myMap[nums[i]]=i;
         }
-        for(int k=0;k<nums.size();k++){
-            if(nums[k]==final[i]||nums[k]==final[j]){
-                answer.push_back(k);
-            }
-        }
-        return answer;
+        return {};
     }
+
 };
