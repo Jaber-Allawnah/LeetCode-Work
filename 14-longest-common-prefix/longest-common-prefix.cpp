d2333f0debdcb1b9278f8f57eb9f29204123fa8a
Jaber Allawnah <s12112071@stu.najah.edu>
2026-05-09 15:53:18 +0300

    Time: 4 ms (6.09%) | Memory: 12.5 MB (6.52%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,63 +1,58 @@
-class TrieNode {
-public:
+class TrieNode{
+    public:
     TrieNode* children[26];
     bool isEnd;
-
-    TrieNode() {
-        isEnd = false;
-        for (int i = 0; i < 26; i++) {
+    TrieNode(){
+        isEnd=false;
+        for(int i = 0; i < 26; i++) {
             children[i] = nullptr;
         }
     }
+    
 };
-
-class Trie {
+class Trie{
     TrieNode* root;
-
-public:
-    Trie() {
-        root = new TrieNode();
+    public:
+    string longestPrefix;
+    Trie(){
+        root=new TrieNode();
+        longestPrefix="";
     }
 
-    void insert(string s) {
-        TrieNode* current = root;
-
-        for (char c : s) {
-            int index = c - 'a';
-
-            if (current->children[index] == nullptr) {
-                current->children[index] = new TrieNode();
+    void insert(string s){
+        TrieNode* current=root;
+        for(char c: s){
+            int index=c-'a';
+            if(current->children[index]!=nullptr){
+                current=current->children[index];
+                continue;
             }
-
-            current = current->children[index];
+            current->children[index]=new TrieNode();
+            current=current->children[index];
         }
-
-        current->isEnd = true;
+        current->isEnd=true;
     }
 
-    string returnLongestPrefix() {
-        string longestPrefix = "";
-        TrieNode* current = root;
-
-        while (current && !current->isEnd) {
-            int count = 0;
-            int index = -1;
-
-            for (int i = 0; i < 26; i++) {
-                if (current->children[i] != nullptr) {
+    string returnLongestPrefix(){
+        int count=0;
+        int index=0;
+        TrieNode* current=root;
+        while(current&&!current->isEnd){
+            for(int j=0;j<26;j++){
+                if(current->children[j]!=nullptr){
                     count++;
-                    index = i;
+                    index=j;
                 }
-            }
 
-            if (count != 1) {
+            }
+            if(count!=1){
                 break;
-            }
-
-            longestPrefix += char('a' + index);
-            current = current->children[index];
+            }  
+                longestPrefix+=char('a'+index);
+                current=current->children[index];
+            count=0;
         }
-
+        
         return longestPrefix;
     }
 };
