aa5dcb3bf0c9491f4a99e92a230b8e54408d36d1
Jaber Allawnah <s12112071@stu.najah.edu>
2026-06-28 10:16:54 +0300

    Time: 7 ms (9.38%) | Memory: 15.7 MB (10.49%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,42 +1,27 @@
 class Solution {
 private:
 vector<vector<int>>final;
-int target;
 vector<int> candidates;
 public:
-    int findSum(vector<int> current){
-        int sum=0;
-        for(int i=0;i<current.size();i++){
-            sum+=current[i];
-        }
-        return sum;
-    }
-    void backTrack(vector<int> current,int i){
-        if(findSum(current)==target){
+    void backTrack(int start,int remaining,vector<int> current){
+        if(remaining==0){
             final.push_back(current);
-            return;
-        }else if(findSum(current)>target){
-            return;
-        }else{
-            for(int j=i+1;j<candidates.size();j++){
-                if(j>i+1&&candidates[j]==candidates[j-1]) continue;
-                current.push_back(candidates[j]);
-                backTrack(current,j);
-                current.pop_back();
-            }
+        }
+        for(int i=start;i<candidates.size();i++){
+            if(i>start&&candidates[i]==candidates[i-1]) continue;
+            if(remaining-candidates[i]<0) break;
+            current.push_back(candidates[i]);
+            backTrack(i+1,remaining - candidates[i],current);
+            current.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
         vector<int> current;
-        this->target=target;
         this->candidates=candidates;
-        for(int i=0;i<candidates.size();i++){
-            if(i>0&&candidates[i]==candidates[i-1]) continue;
-            current.push_back(candidates[i]);
-            backTrack(current,i);
-            current.pop_back();
-        }
+
+        backTrack(0,target,{});
+        
         return final;
     }
 };
